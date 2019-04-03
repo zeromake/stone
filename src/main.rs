a59659754e9a53464b92c012065a9c088e6c00b8
zeromake <a390720046@gmail.com>
2019-04-03 14:54:56 +0800

    fix: add idea project config

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
         r"(0[0-7]+)",
         // hex
         r"(0x[0-9a-fA-F]+)",
-        // flost
+        // float
         r"([0-9]+.[0-9]+)",
         // int
         r"([0-9]+)",
@@ -20,13 +20,13 @@ fn main() {
         // variable
         r"[A-z_a-z][A-z_a-z0-9]*",
         r"\.",
-        // 
+        //
         r"\{|\}|\(|\)",
         // 计算语句
         r"=|==|>=|<=|>|<|&&|\|\||&|\|)?",
     ];
-    let regex_str = &arr.join("|");
-    let regex_pat = Regex::new(regex_str).unwrap();
+    let regex_str = arr.join("|");
+    let regex_pat = Regex::new(&regex_str).unwrap();
     let code_str = r#"let test = 'ff'"#;
     for caps in regex_pat.captures_iter(code_str) {
         let cap = caps.get(1);
